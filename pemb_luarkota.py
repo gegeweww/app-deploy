4839d919f067e140b27dfcb9fc7130e5c56f8cd0
gegeweww <125875278+gegeweww@users.noreply.github.com>
2025-08-25 15:16:08 +0700

    Update pemb_luarkota.py

--- pemb_luarkota.py
+++ pemb_luarkota.py
@@ -96,7 +96,7 @@ def run():
                 sisa_baru = round(total_terbayar - total, 2)
                 status_baru = "Lunas" if sisa_baru >= 0 else "Belum Lunas"
 
-                tanggal_bayar_str = tanggal_bayar.strftime("%d-%m-%y)
+                tanggal_bayar_str = tanggal_bayar.strftime("%d-%m-%Y)
                 timestamp = datetime.now(ZoneInfo("Asia/Jakarta")).strftime("%d-%m-%Y %H:%M:%S")
                 ke = df_all.shape[0] + 1
                 user = st.session_state.get("user", "Unknown")
